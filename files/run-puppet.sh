97ef3984d4d711dc8f17c17027097ed2ebf23395
Billiamness <billiammellon@gmail.com>
2018-11-13 18:14:26 -0500

    run puppet sh change hello no maow

--- files/run-puppet.sh
+++ files/run-puppet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/billy/Documents/puppetAgent/puppetTutorial/ && git pull
-/home/billy/Documents/puppetAgent/puppetTutorial/ apply manifests/
+/home/billy/Documents/puppetAgent/puppetTutorial apply manifests/
