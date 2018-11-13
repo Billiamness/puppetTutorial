f687f8778a0563a42c2e2d86be7a5374606f35c8
Billiamness <billiammellon@gmail.com>
2018-11-13 17:17:39 -0500

    attempt 2

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -1,6 +1,6 @@
 # Set up regular Puppet runs
 file { '/usr/local/bin/run-puppet':
-  source => '/home/billy/Documents/puppet/files/run-puppet.sh',
+  source => '/home/billy/Documents/puppetAgent/puppetTutorial/files/run-puppet.sh',
   mode   => '0755',
 }
 
