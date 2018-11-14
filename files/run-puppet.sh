3ac27de52d00967a88487a6c3bd25f6eaad2a8c5
Billiamness <billiammellon@gmail.com>
2018-11-14 11:25:49 -0500

    run puppet sh etc/puppet

--- files/run-puppet.sh
+++ files/run-puppet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/billy/Documents/puppetAgent/puppetTutorial/ && git pull
-/home/billy/Documents/puppetAgent/puppetTutorial apply manifests/
+cd /home/billy/Documents/puppetAgent/puppetTutorial && git pull
+/etc/puppet apply manifests/
